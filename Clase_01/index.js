8e006eb6d8e9a447f90bbd276d4287c443e9d218
martinmuchut <martin_muchut@hotmail.com>
2022-09-01 15:40:40 -0300

    actualizado

---
+++ Clase_01/index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const PORT = 3000;
+const app = express();
+
+app.listen(PORT, () => {
+    console.log('Servidor trabajando')
+});
+
+
+
+
+console.log('Hola Mundo')
